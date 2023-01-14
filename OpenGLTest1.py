a24ee1e65c136a3ab0055968cc875def26be3f1c
Alice Williams <alice.williams.tech@gmail.com>
2023-01-14 11:30:18 -0800

    Add GL version logging

--- OpenGLTest1.py
+++ OpenGLTest1.py
@@ -7,6 +7,8 @@ OpenGL.ERROR_LOGGING = True
 # Full Error traces calls to OpenGL logging module for error trace
 OpenGL.FULL_LOGGING = False
 
+GL_INFO_LOG = True
+
 import sys
 from OpenGL.GL   import *
 from OpenGL.GLUT import *
@@ -20,6 +22,8 @@ class OpenGLEnv:
         glutInitWindowSize(640, 480)
         self.window = glutCreateWindow('PyOpenGL Test')
         self.setup_viewport()
+        if GL_INFO_LOG:
+            self.gl_info()
         glutReshapeFunc(self.reshape)
         glutIdleFunc(self.display)
         glutMouseFunc(self.mouse)
@@ -29,13 +33,14 @@ class OpenGLEnv:
     
     def close(self):
         if self.window:
+            print('Program closing.')
             glutDestroyWindow(self.window)
 
     def setup_viewport(self):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(0.0, 1.0, 0.0, 1.0, 0.0, 1.0)
-        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearColor(0.75, 0.75, 0.75, 0.0)
 
     def reshape(self, width, height):
         glViewport(0, 0, width, height)
@@ -51,6 +56,19 @@ class OpenGLEnv:
 
     def mouse(self, button, state, x, y):
         pass
+
+    def gl_info(self):
+        print('PyOpenGL Version: ', OpenGL.__version__)
+        print('GL Vendor: ',        glGetString(GL_VENDOR).decode())
+        print('GL Renderer: ',      glGetString(GL_RENDERER).decode())
+        print('GL Version: ',       glGetString(GL_VERSION).decode())
+        exts = glGetString(GL_EXTENSIONS)
+        if exts is None:
+            print('GL Extensions: ', exts.decode())
+        else:
+            print('GL Extensions:')
+            for ext in exts.split():
+                print('\t', ext.decode())
 
 if __name__ == "__main__":
     print('Press any key to quit.')
